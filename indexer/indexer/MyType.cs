d08e0c3a5534bb60bc8d4518b021e6b924a0c29f
emilhajizade42 <6xp3xcg@code.edu.az>
2022-04-08 12:12:38 +0400

    must be

---
+++ indexer/indexer/MyType.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace indexer
+{
+    internal class MyType<T>:IEnumerator,IEnumerable,IComparable<MyType<T>>
+    {
+        public T[] arr ;
+        
+        public T this[int index]
+            {
+                get
+                {
+                    return arr[index];
+                }
+                set
+                {
+                    arr[index] = value;
+                }
+            }
+        int position = -1;
+
+
+
+
+        public void Add(T item)
+        {
+            Array.Resize(ref arr, arr.Length+1);
+            arr[arr.Length-1] = item;
+
+        }
+        public void Resizeable(bool a)
+        {
+         
+        }
+     
+
+
+        public MyType( )
+        {
+            arr = new T[0];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+        //IEnumerator
+        public bool MoveNext()
+        {
+            position++;
+            return (position < arr.Length);
+        }
+        //IEnumerable
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public int CompareTo([AllowNull] MyType<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        //IEnumerable
+        public object Current
+        {
+            get { return arr[position]; }
+        }
+    }
+}
---
+++ indexer/indexer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          
+            var a = new MyType<int>();
+            a.Add(1);
+            a.Add(5);
+            a.Add(15);
+            a.Add(8);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
